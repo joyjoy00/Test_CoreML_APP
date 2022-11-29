30ec6192052aaefd7019099118f204cde7a581b9
joyjoy00 <eunseo8946@yonsei.ac.kr>
2022-11-29 10:47:45 +0900

    first commit

---
+++ BlazePose_test2/BlazePose_test2App.swift
@@ -0,0 +1,17 @@
+//
+//  BlazePose_test2App.swift
+//  BlazePose_test2
+//
+//  Created by 정은서 on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct BlazePose_test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlazePose_test2/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BlazePose_test2
+//
+//  Created by 정은서 on 2022/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        PrivateSleepView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
